057049c1994e27d2c85b8030bca98c35b1767bc9
William Simmons <willsimmons@gmail.com>
2016-08-31 11:06:07 -0700

    react example tests

--- app.js
+++ app.js
@@ -43,29 +43,71 @@
 // ReactDOM.render(<App/>,document.getElementById("app"));
 
 //Rendering Dynamically Generated Components
-class GroceryListItem extends React.Component {
-  //each item can be called with a property
-  constructor(props) {
-    super(props);
-  }
-  //this, will be the item that is dynamically passed via props
-  render() {
-    return (
-      <li>{this.props.item}</li>
-      );
-  }
-}
+// class GroceryListItem extends React.Component {
+//   //each item can be called with a property
+//   constructor(props) {
+//     super(props);
+//   }
+//   //this, will be the item that is dynamically passed via props
+//   render() {
+//     return (
+//       <li>{this.props.item}</li>
+//       );
+//   }
+// }
 
-//this static, as in it's one list, it's the items that change
+// //this static, as in it's one list, it's the items that change
+// var GroceryList = (props) => (
+//   <ul>
+  
+//   {console.log(props)}
+//     {
+//     props.item.map((item,index) =>
+//       <GroceryListItem key={index} item={item} />
+//   )}
+//   </ul>
+// );
+// 
+//states in components determine what's viewed at a particular time
+//state is an object
+class GroceryListItem extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state ={
+      done:false
+    };
+  }
+
+  //attaching an event to change state
+  //changing state on each click 
+  onListItemClick(){
+    this.setState({
+      done: !this.state.done
+    })
+  }
+  
+  render(){
+    //the style of the render changes depending on state
+    var style ={
+      //if done is true, line through, else none
+      //notice that this is CSS'y
+      textDecoration: this.state.done ? "line-through" : "none"
+    };
+    return(
+      <li style ={style} onClick={this.onListItemClick.bind(this)}> {this.props.item} </li>
+    );
+  }
+};
+
 var GroceryList = (props) => (
   <ul>
-  
-  {console.log(props)}
-    {
-    props.item.map((item,index) =>
-      <GroceryListItem key={index} item={item} />
+  {
+    props.item.map(
+      (item,index) => <GroceryListItem key={index} item={item}/>
   )}
   </ul>
 );
+
 
 ReactDOM.render(<GroceryList item={['learn react','build something','profit']}/>,document.getElementById("app"));
